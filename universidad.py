c1e4e13720e0479bc3b2e965a70361334e6b2584
w4ity <waityt3stt@gmail.com>
2024-03-15 17:37:43 +0100

    UML parece completo, codigo apenas sin tocar, + jpg del uml

--- universidad.py
+++ universidad.py
@@ -1,6 +1,41 @@
+from enum import Enum
+
 class Persona:
     def __init__(self,nombre, dni, direccion, sexo):
         self.nombre = nombre
         self._dni = dni
         self._direccion = direccion
         self.sexo = sexo
+
+    def get_nombre():
+
+    
+    def get_dni():
+
+    
+    def get_direccion():
+    
+    def get_sexo():
+
+
+class Profesor(Persona):
+    def __init__(self, nombre,dni, direccion, sexo, asignaturas):
+        super().__init__(nombre, dni, direccion, sexo)
+        self.asignaturas = asignaturas
+
+
+    def add_miembro():
+
+    def del_miembro():
+
+    def cambio_depart():
+
+    def add_asignatura():
+
+    def del_asignatura():
+
+
+class Departamento(Enum):
+     
+
+
